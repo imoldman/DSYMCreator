ec12503adb0c107b19676433f98b0b016ca2df03
lixianliang <lixianliang@corp.netease.com>
2016-08-31 13:21:40 +0800

    Initial Commit

---
+++ DSYMCreator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DSYMCreator
+//
+//  Created by oldman on 8/31/16.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
